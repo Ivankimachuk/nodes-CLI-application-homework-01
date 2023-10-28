f87358872c01c9fc52756492f241a88044e5b3c3
Ivankimachuk <123023827+Ivankimachuk@users.noreply.github.com>
2023-10-28 21:11:16 +0300

    HMK

---
+++ contacts.js
@@ -0,0 +1,33 @@
+const fs = require("node:fs/promises");
+const path = require("node: path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+    // We return the data
+    const data = await fs.readFile(contactsPath, 'utf-8')
+    console.log(listContacts);
+}
+listContacts()
+
+// /*
+//  * Розкоментуй і запиши значення
+//  * const contactsPath = ;
+//  */
+
+// // TODO: задокументувати кожну функцію
+// function listContacts() {
+//     // ...твій код. Повертає масив контактів.
+//   }
+
+//   function getContactById(contactId) {
+//     // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+//   }
+
+//   function removeContact(contactId) {
+//     // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+//   }
+
+//   function addContact(name, email, phone) {
+//     // ...твій код. Повертає об'єкт доданого контакту.
+//   }
---
+++ index.js
@@ -0,0 +1,5 @@
+function sum(a, b, c) {
+  return a + b + c;
+}
+console.log(sum(2, 2, 7));
+console.log(sum(6, 6, 2));
